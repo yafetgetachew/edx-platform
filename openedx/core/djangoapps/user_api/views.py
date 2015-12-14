00b9554f893d16385a410e6eb1f4452066ebcbd9
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2015-12-14 15:29:00 +0000

    Update marketing links

--- openedx/core/djangoapps/user_api/views.py
+++ openedx/core/djangoapps/user_api/views.py
@@ -618,10 +618,10 @@ class RegistrationView(APIView):
         else:
             # Translators: This is a legal document users must agree to
             # in order to register a new account.
-            terms_text = _(u"Terms of Service and Honor Code")
+            terms_text = _(u"Terms of Service")
 
         terms_link = u"<a href=\"{url}\">{terms_text}</a>".format(
-            url=marketing_link("HONOR"),
+            url=reverse('tos'),
             terms_text=terms_text
         )
 
