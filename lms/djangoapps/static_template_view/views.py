c84b9f7338010ac5843597d779d153457889a4e4
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2015-12-14 13:07:49 +0000

    Add feedback form

--- lms/djangoapps/static_template_view/views.py
+++ lms/djangoapps/static_template_view/views.py
@@ -12,6 +12,12 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 from util.cache import cache_if_anonymous
 
+# Additional imports for feedback form
+from static_template_view.forms import FeedbackForm
+from django.core.mail import send_mail
+from smtplib import SMTPException
+from django.middleware.csrf import get_token
+
 valid_templates = []
 
 if settings.STATIC_GRAB:
@@ -39,7 +45,45 @@ def render(request, template):
 
     url(r'^jobs$', 'static_template_view.views.render', {'template': 'jobs.html'}, name="jobs")
     """
-    return render_to_response('static_templates/' + template, {})
+
+    notification = None
+    style = ''
+    context = {}
+
+    if settings.FEATURES.get('USE_CUSTOM_THEME', False):
+        if request.method == 'POST':
+            form = FeedbackForm(request.POST)
+            if form.is_valid():
+                data_form = form.cleaned_data
+                subject = 'feedback'
+
+                full_message = \
+                u'Full name: {full_name} \nEmail: {email} \nPhone: {phone} \nI am a: {i_am_a} \nInquiry type: {inquiry_type} \n{message} '.format(
+                    full_name=data_form['full_name'],
+                    email=data_form['email'],
+                    phone=data_form.get('phone', ''),
+                    i_am_a=data_form.get('i_am_a', ''),
+                    inquiry_type=data_form['inquiry_type'],
+                    message=data_form['message']
+                    )
+                try:
+                    send_mail(subject, full_message, data_form['email'], [settings.TECH_SUPPORT_EMAIL,], fail_silently=False,)
+                    notification = 'Message was successfuly sent. Thank you for contacting us!'
+                except SMTPException as e:
+                    notification = 'Message not been sent, please try again later'
+                style = 'style="margin-top:10px;"'
+        else:
+            form = FeedbackForm()
+
+        csrf_token = get_token(request)
+        context = {
+            'csrf_token': csrf_token,
+            'message': notification,
+            'style': style,
+            'form': form
+        }
+
+    return render_to_response('static_templates/' + template, context)
 
 
 @ensure_csrf_cookie
