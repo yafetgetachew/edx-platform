824d2c6bc98c64a2d73f61471f877157e099d759
sendr <sendr84@gmail.com>
2016-01-22 10:37:17 +0200

    attempt to fix userprofile matching query does not exist

--- common/djangoapps/student/models.py
+++ common/djangoapps/student/models.py
@@ -1412,7 +1412,8 @@ def enforce_single_login(sender, request, user, signal, **kwargs):    # pylint: 
             key = request.session.session_key
         else:
             key = None
-        user.profile.set_login_session(key)
+        if user and hasattr(user, 'profile'):
+            user.profile.set_login_session(key)
 
 
 class DashboardConfiguration(ConfigurationModel):
