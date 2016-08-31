af6fd22d81de85b6fa0d17a7a0d281828cdc2e3d
Rene Sorel <rene@proversity.org>
2016-08-31 12:59:25 +0200

    display default checkout currency correctly

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -109,10 +109,18 @@ from lms.envs.common import (
 
     CONTACT_EMAIL,
 
+<<<<<<< HEAD
     DISABLE_ACCOUNT_ACTIVATION_REQUIREMENT_SWITCH,
     # Video Image settings
     VIDEO_IMAGE_SETTINGS,
+<<<<<<< HEAD
     VIDEO_TRANSCRIPTS_SETTINGS,
+=======
+=======
+    # Setting for PAID_COURSE_REGISTRATION, DOES NOT AFFECT VERIFIED STUDENTS
+    PAID_COURSE_REGISTRATION_CURRENCY
+>>>>>>> display default checkout currency correctly
+>>>>>>> display default checkout currency correctly
 )
 from path import Path as path
 from warnings import simplefilter
