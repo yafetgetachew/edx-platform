3eca801fef5b1baf0b52e057f82a674a16254126
xahgmah <xahgmah@yandex.ru>
2016-09-05 12:50:10 +0300

    send metric inside celery tasks

--- common/djangoapps/gamification_metric/signals.py
+++ common/djangoapps/gamification_metric/signals.py
@@ -3,7 +3,7 @@ Catch changes in user progress and send it by API
 """
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from api_calls import APICalls
+from tasks import send_api_request
 
 
 @receiver(post_save, sender='courseware.StudentModule')
@@ -15,4 +15,4 @@ def send_achivenent(sender, instance, **kwargs):
             'event_type': instance.module_type,
             'uid': unicode(instance.module_state_key),
         }
-        APICalls().api_call(**data)
+        send_api_request(data)
