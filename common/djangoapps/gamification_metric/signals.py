f8ec0e245d77e0c4751f925985656c144192f4e3
xahgmah <xahgmah@yandex.ru>
2016-09-07 16:08:49 +0300

    filter calls

--- common/djangoapps/gamification_metric/signals.py
+++ common/djangoapps/gamification_metric/signals.py
@@ -1,14 +1,22 @@
 """
 Catch changes in user progress and send it by API
 """
+import json
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from tasks import send_api_request
+
 
 
 @receiver(post_save, sender='courseware.StudentModule')
 def send_achivenent(sender, instance, **kwargs):
     if instance.module_type in ('video', 'problem', 'course'):
+        if instance.module_type == 'video' and (instance.modified-instance.created).total_seconds()<=1:
+            return None
+        if instance.module_type == 'course' and json.loads(instance.state).get('position'):
+            return None
+        if instance.module_type == 'problem' and (not instance.grade or type(instance.grade) != float):
+            return None
         data = {
             'username': instance.student.username,
             'course_id': unicode(instance.course_id),
