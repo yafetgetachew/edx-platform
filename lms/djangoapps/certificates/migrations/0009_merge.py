e8f2a9c97be484b3df4437354f85cab976aebf39
Dmitry Gamanenko <dgamanenko@gmail.com>
2016-09-19 17:53:56 +0000

    merging certificate migrations

---
+++ lms/djangoapps/certificates/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('certificates', '0006_auto_20160325_0541'),
+        ('certificates', '0008_schema__remove_badges'),
+    ]
+
+    operations = [
+    ]
