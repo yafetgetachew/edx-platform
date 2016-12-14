5fcc03778479a61a742d33621a6a7e687df5ca9d
Jeremy Bowman <jbowman@edx.org>
2016-12-14 14:47:03 -0500

    PLAT-1104 Import courses asynchronously

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2185,6 +2185,9 @@ CSRF_COOKIE_SECURE = False
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'openedx.core.lib.api.paginators.DefaultPagination',
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'PAGE_SIZE': 10,
     'URL_FORMAT_OVERRIDE': None,
 }
