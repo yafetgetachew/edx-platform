3dadee69f906e4bfaa314a4d714d68862e9d26d3
DevOps <DevOps@raccoongang.com>
2016-12-14 12:50:21 +0000

    Add ExtHostElasticsearch class for remote elasticsearch

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -29,6 +29,8 @@ import os
 from path import Path as path
 from xmodule.modulestore.modulestore_settings import convert_module_store_setting_if_needed
 
+from elasticsearch import Elasticsearch
+
 # SERVICE_VARIANT specifies name of the variant used, which decides what JSON
 # configuration files are read during startup.
 SERVICE_VARIANT = os.environ.get('SERVICE_VARIANT', None)
@@ -870,3 +872,14 @@ DOC_LINK_BASE_URL = ENV_TOKENS.get('DOC_LINK_BASE_URL', DOC_LINK_BASE_URL)
 ################ CUSTOM CONFIGURATIONS ################
 
 COPYRIGHT_YEAR = ENV_TOKENS.get('COPYRIGHT_YEAR', COPYRIGHT_YEAR)
+
+##################### Raccoongang changes #####################
+ELASTIC_SEARCH_HOST = ENV_TOKENS.get('ELASTIC_SEARCH_HOST', 'localhost')
+
+class ExtHostElasticsearch(Elasticsearch):
+    def __init__(self, *args, **kwargs):
+        kwargs['host'] = ELASTIC_SEARCH_HOST
+        super(ExtHostElasticsearch, self).__init__(*args, **kwargs)
+
+ELASTIC_SEARCH_IMPL = ExtHostElasticsearch
+
