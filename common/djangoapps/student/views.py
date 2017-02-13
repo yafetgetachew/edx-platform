260d3a805b6483eb11587386d005a78546277903
madeira <devops@raccoongang.com>
2017-02-13 14:38:52 +0200

    Add "Featured programs to index page".

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -120,7 +120,7 @@ from notification_prefs.views import enable_notifications
 
 from openedx.core.djangoapps.credit.email_utils import get_credit_provider_display_names, make_providers_strings
 from openedx.core.djangoapps.user_api.preferences import api as preferences_api
-from openedx.core.djangoapps.programs.utils import get_programs_for_dashboard, get_display_category
+from openedx.core.djangoapps.programs.utils import get_programs_for_dashboard, get_display_category, ProgramProgressMeter
 from openedx.core.djangoapps.programs.models import ProgramsApiConfig
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from openedx.core.djangoapps.theming import helpers as theming_helpers
@@ -175,6 +175,30 @@ def index(request, extra_context=None, user=AnonymousUser()):
 
     context = {'courses': courses}
 
+    programs_config = ProgramsApiConfig.current()
+
+    if not request.user.is_authenticated():
+        user, _ = User.objects.get_or_create(
+            username='programs_dummy_user_for_api'
+        )
+    else:
+        user = request.user
+
+    meter = ProgramProgressMeter(user)
+    programs = filter(lambda p: p.get('featured'), meter.programs)
+
+    for program in programs:
+        program['detail_url'] = reverse(
+            'xseries:program_marketing',
+            kwargs={'program_id': program['id']}
+        ).rstrip('/')
+        program['display_category'] = get_display_category(program)
+
+    context.update({
+        'programs': programs,
+        'show_program_listing': programs_config.show_program_listing,
+    })
+
     context['homepage_overlay_html'] = configuration_helpers.get_value('homepage_overlay_html')
 
     # This appears to be an unused context parameter, at least for the master templates...
@@ -192,6 +216,8 @@ def index(request, extra_context=None, user=AnonymousUser()):
 
     # allow for theme override of the courses list
     context['courses_list'] = theming_helpers.get_template_path('courses_list.html')
+
+    context['programs_list'] = theming_helpers.get_template_path('programs_list.html')
 
     # Insert additional context for use in the template
     context.update(extra_context)
