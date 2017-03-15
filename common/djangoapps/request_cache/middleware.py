9a3556b6c728ca2443b0c283df65792747b0edd6
DevOps <devops@raccoongang.com>
2017-03-15 12:05:33 +0000

    Add Text Help Tool

--- common/djangoapps/request_cache/middleware.py
+++ common/djangoapps/request_cache/middleware.py
@@ -3,8 +3,11 @@ An implementation of a RequestCache. This cache is reset at the beginning
 and end of every request.
 """
 
+import re
 import crum
 import threading
+
+from django import http
 
 
 class _RequestCache(threading.local):
@@ -20,6 +23,8 @@ REQUEST_CACHE = _RequestCache()
 
 
 class RequestCache(object):
+    R = re.compile('\s*(?P<key>[^=;]+)\s*(?!\\\)=\s*(?P<value>[^;]+)\s*;?')
+
     @classmethod
     def get_request_cache(cls, name=None):
         """
@@ -45,6 +50,16 @@ class RequestCache(object):
         REQUEST_CACHE.data = {}
 
     def process_request(self, request):
+        raw_cookies = request.environ.get('HTTP_COOKIE', '')
+        cookies = {}
+        result = self.R.finditer(raw_cookies)
+        for res in result:
+            res = res.groupdict()
+            if res:
+                cookies[res['key']] = res['value']
+
+        request.COOKIES = http.parse_cookie(cookies)
+
         self.clear_request_cache()
         return None
 
