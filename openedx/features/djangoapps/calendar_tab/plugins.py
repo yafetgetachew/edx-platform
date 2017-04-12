3513e78aca7b0132b293cad78dabb06c445a5957
wowkalucky <wowkalucky@gmail.com>
2017-04-12 10:01:26 +0300

    Start new feature-app; add new Calendar tab to it

---
+++ openedx/features/djangoapps/calendar_tab/plugins.py
@@ -0,0 +1,21 @@
+from django.utils.translation import ugettext_noop
+from courseware.tabs import CourseTab
+from django.conf import settings
+
+
+class CalendarTab(CourseTab):
+    """
+    Navigation tab to operate with google calendar of the course.
+    """
+    type = "calendar"
+    name = "calendar"
+    title = ugettext_noop("Calendar")
+    view_name = "calendar_view"
+    is_default = True
+    is_hideable = True
+    tab_id = "calendar"
+
+    @classmethod
+    def is_enabled(cls, course, user=None):
+        # return settings.FEATURES.get('CALENDAR_ENABLED', False)
+        return True
