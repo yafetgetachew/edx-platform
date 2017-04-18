8b169495b15d3cef7b5be97b6ab32c6ddaecfe1f
wowkalucky <wowkalucky@gmail.com>
2017-04-19 02:49:20 +0300

    ALFA: Perform google calendar event creation via lms calendar tab

--- openedx/features/djangoapps/calendar_tab/forms.py
+++ openedx/features/djangoapps/calendar_tab/forms.py
@@ -1,15 +1,41 @@
+# -*- coding: utf-8 -*-
 from django import forms
+
+from oauth2client.service_account import ServiceAccountCredentials
+from apiclient.discovery import build
 
 
 class EventForm(forms.Form):
     """Form for Google Calendar event creation"""
     title = forms.CharField()
     description = forms.CharField(widget=forms.Textarea, required=False)
+    location = forms.CharField(widget=forms.Textarea, required=False)
     start = forms.DateTimeField()
     end = forms.DateTimeField()
 
     def create_event(self):
         """Performs Google Calendar API event.insert request"""
-        print(self.cleaned_data)
-        print('API REQUEST')
-        pass
+
+        scopes = ['https://www.googleapis.com/auth/calendar']
+        # TODO: make credentials file configurable
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            '/edx/app/edxapp/edx-platform/openedx/features/djangoapps/calendar_tab/openedx-google-calendar-private-key.json',
+            scopes)
+
+        gcal_service = build('calendar', 'v3', credentials=credentials)
+
+        # TODO: make calendar "dynamic"
+        calendar_id = "o5i5gqd2s4sa3ngtt8td3qbqsg@group.calendar.google.com"
+        event = {
+            'summary': self.cleaned_data.get('title'),
+            'location': self.cleaned_data.get('location') or '',
+            'description': self.cleaned_data.get('description') or '',
+            'start': {
+                'dateTime': self.cleaned_data.get('start').isoformat(),
+            },
+            'end': {
+                'dateTime': self.cleaned_data.get('end').isoformat(),
+            },
+        }
+
+        gcal_service.events().insert(calendarId=calendar_id, body=event).execute()
--- openedx/features/djangoapps/calendar_tab/views.py
+++ openedx/features/djangoapps/calendar_tab/views.py
@@ -1,11 +1,10 @@
-from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse_lazy
-from django.views.decorators.http import require_POST
 from django.views.generic.edit import FormView
 
 from opaque_keys.edx.keys import CourseKey
 from courseware.courses import get_course_with_access
+from courseware.access import has_access
 
 from .forms import EventForm
 
@@ -21,11 +20,42 @@ class CalendarView(FormView):
     def get_context_data(self, **kwargs):
         course_key = CourseKey.from_string(self.kwargs['course_id'])
         course = get_course_with_access(self.request.user, "load", course_key)
+        is_staff = bool(has_access(self.request.user, 'staff', course))
 
         context = super(CalendarView, self).get_context_data(**kwargs)
         context['course'] = course
         context['csrf_token'] = csrf(self.request)["csrf_token"]
+        context['is_staff'] = is_staff
         return context
 
     def get_success_url(self):
         return reverse_lazy('calendar_view', kwargs={'course_id': self.kwargs['course_id']})
+
+
+def create_course_calendar(request):
+    """Creates google calendar and associates it with course"""
+
+    # # create new calendar:
+    # calendar = {
+    #     'summary': 'calendarSummary',
+    #     'timeZone': 'America/Los_Angeles'
+    # }
+    # created_calendar = gcal_service.calendars().insert(body=calendar).execute()
+    # print created_calendar['id']
+    pass
+
+
+def share_course_calendar(request):
+    """Shares course google calendar to provided google account"""
+
+    # make calendar public:
+    # rule = {
+    #     'scope': {
+    #         'type': 'default',
+    #     },
+    #     'role': 'reader'
+    # }
+    #
+    # created_rule = gcal_service.acl().insert(calendarId=calendar_id, body=rule).execute()
+    # print created_rule['id']
+    pass
