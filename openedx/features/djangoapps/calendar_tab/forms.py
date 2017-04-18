10ccd3d95cfb92030618ad8f00eb69e4015bd7f9
wowkalucky <wowkalucky@gmail.com>
2017-04-19 02:49:20 +0300

    TO AMEND: bootstrap google calendar api request (returns 401 - login required)

--- openedx/features/djangoapps/calendar_tab/forms.py
+++ openedx/features/djangoapps/calendar_tab/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from apiclient import discovery
 
 
 class EventForm(forms.Form):
@@ -11,5 +12,23 @@ class EventForm(forms.Form):
     def create_event(self):
         """Performs Google Calendar API event.insert request"""
         print(self.cleaned_data)
-        print('API REQUEST')
-        pass
+
+        key = "AIzaSyCMoABDkdDl_74tg8FebGRRy3a4Nbk97-U"
+        calendar_id = "raccoongang.com_1ihbicvrp826c79c331lk1a0qo@group.calendar.google.com"
+
+        gcal_service = discovery.build('calendar', 'v3', developerKey=key)
+        event = {
+            'summary': 'Google I/O 2017',
+            'location': '800 Howard St., San Francisco, CA 94103',
+            'description': 'A chance to hear more about Google.',
+            'start': {
+                'dateTime': '2017-04-19T05:00:00-03:00',
+            },
+            'end': {
+                'dateTime': '2017-04-19T15:00:00-03:00',
+            },
+        }
+
+        event = gcal_service.events().insert(calendarId=calendar_id, body=event).execute()
+        print 'Event created: %s' % (event.get('htmlLink'))
+        print(dir(event))
