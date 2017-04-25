956a20840ba5fe7048c18f5d88c33a881fa4c87a
wowkalucky <wowkalucky@gmail.com>
2017-04-25 12:47:19 +0300

    Move google calendar API key to FEATURES settings

--- openedx/features/djangoapps/calendar_tab/utils.py
+++ openedx/features/djangoapps/calendar_tab/utils.py
@@ -1,12 +1,19 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
 from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
 
 scopes = ['https://www.googleapis.com/auth/calendar']
 
-# TODO: add some consperancy here:
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            '/edx/app/edxapp/edx-platform/openedx/features/djangoapps/calendar_tab/openedx-google-calendar-private-key.json',
-            scopes)
+try:
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        settings.FEATURES['GOOGLE_CALENDAR_TAB_PRIVATE_KEY_URL'], scopes)
+except (IOError, KeyError) as e:
+    raise ImproperlyConfigured(
+        "You must set `FEATURES['GOOGLE_CALENDAR_TAB_PRIVATE_KEY_URL']` when "
+        "`FEATURES['ENABLE_CALENDAR']` is True."
+    )
 
 gcal_service = build('calendar', 'v3', credentials=credentials)
 
