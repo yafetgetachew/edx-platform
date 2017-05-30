df4465ca5afaf96a951e2a5f7876e6b2eb7e11f2
Nico van Niekerk <nico.niekerk@gmail.com>
2017-05-30 13:50:33 +0200

    UPD: adding submissions endoint url

--- lms/urls.py
+++ lms/urls.py
@@ -42,7 +42,11 @@ urlpatterns = (
 	# Submissions api
     url(r'^api/submissions/', include('submissions.urls')),
 	
+<<<<<<< HEAD
     # Event tracking endpoints
+=======
+	# Event tracking endpoints
+>>>>>>> UPD: adding submissions endoint url
     url(r'', include('track.urls')),
 
     # Performance endpoints
