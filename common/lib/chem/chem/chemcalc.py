7d7919f6f27a318c702fad3d17b51ead506d77ab
Nico van Niekerk <nico.niekerk@gmail.com>
2017-09-07 14:33:30 +0200

    reverting nltk change

--- common/lib/chem/chem/chemcalc.py
+++ common/lib/chem/chem/chemcalc.py
@@ -54,7 +54,7 @@ grammar = """
 
   suffixed -> unsuffixed | unsuffixed suffix
 """
-parser = nltk.ChartParser(nltk.parse_cfg(grammar))
+parser = nltk.ChartParser(CFG.fromstring(grammar))
 
 
 def _clean_parse_tree(tree):
