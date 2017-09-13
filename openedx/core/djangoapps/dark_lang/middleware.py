26544125e4c86b9de555ca144216d5cc2d65f2ca
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2017-09-13 08:08:14 -0400

    Add site language config

--- openedx/core/djangoapps/dark_lang/middleware.py
+++ openedx/core/djangoapps/dark_lang/middleware.py
@@ -14,6 +14,7 @@ from django.utils.translation import LANGUAGE_SESSION_KEY
 import locale
 from django.utils import translation
 
+from openedx.core.djangoapps.site_configuration.helpers import get_value
 from openedx.core.djangoapps.dark_lang import DARK_LANGUAGE_KEY
 from openedx.core.djangoapps.dark_lang.models import DarkLangConfig
 from openedx.core.djangoapps.user_api.preferences.api import (
@@ -82,7 +83,13 @@ class DarkLangMiddleware(object):
             return
 
         self._clean_accept_headers(request)
+        self._set_site_or_microsite_language(request)
         self._activate_preview_language(request)
+
+    def _set_site_or_microsite_language(self, request):
+        language = get_value('LANGUAGE_CODE', None)
+        if language:
+            request.session[LANGUAGE_SESSION_KEY] = language
 
     def _fuzzy_match(self, lang_code):
         """Returns a fuzzy match for lang_code"""
