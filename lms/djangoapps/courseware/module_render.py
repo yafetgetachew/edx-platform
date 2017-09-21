2dd657e1f10178b355406b89a6147034705d09da
Oksana Slusarenro <oksana.slu@gmail.com>
2017-09-21 16:16:51 +0300

    fix download scorm file

--- lms/djangoapps/courseware/module_render.py
+++ lms/djangoapps/courseware/module_render.py
@@ -982,7 +982,10 @@ def _invoke_xblock_handler(request, course_id, usage_id, handler, suffix, course
             with tracker.get_tracker().context(tracking_context_name, tracking_context):
                 resp = instance.handle(handler, req, suffix)
                 is_prereq = gating_api.is_prerequisite(course_id, instance._parent_block.parent)
-                r = json.loads(resp.app_iter[0])
+                try:
+                    r = json.loads(resp.app_iter[0])
+                except TypeError:
+                    r = {}
                 if is_prereq and r.get('current_score', 0) >= r.get('total_possible', 1):
                     resp = append_data_to_webob_response(resp, {'refresh_page': True})
                 if suffix == 'problem_check' \
