5980aa62a4e5ec622e7afcc995faa9221f0efe03
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2017-11-01 15:16:27 -0400

    added variables and urls for ORA2 functional

--- lms/urls.py
+++ lms/urls.py
@@ -967,3 +967,7 @@ if settings.FEATURES.get('ENABLE_FINANCIAL_ASSISTANCE_FORM'):
             name='submit_financial_assistance_request'
         )
     )
+
+from openassessment.fileupload.urls import urlpatterns as oraurlpatterns
+urlpatterns+= oraurlpatterns
+
