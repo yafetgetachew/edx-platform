dadfa7366728749f8eea8a804bcc80b2df3431f4
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2017-11-17 14:42:46 +0000

    Fix login

--- lms/urls.py
+++ lms/urls.py
@@ -124,15 +124,15 @@ urlpatterns += (
 if settings.FEATURES["ENABLE_COMBINED_LOGIN_REGISTRATION"]:
     # Backwards compatibility with old URL structure, but serve the new views
     urlpatterns += (
-#        url(r'^login$', 'student_account.views.login_and_registration_form',
-#            {'initial_mode': 'login'}, name="signin_user"),
+        url(r'^login$', 'student_account.views.login_and_registration_form',
+            {'initial_mode': 'login'}, name="signin_user"),
         url(r'^register$', 'student_account.views.login_and_registration_form',
             {'initial_mode': 'register'}, name="register_user"),
     )
 else:
     # Serve the old views
     urlpatterns += (
-#        url(r'^login$', 'student.views.signin_user', name="signin_user"),
+        url(r'^login$', 'student.views.signin_user', name="signin_user"),
         url(r'^register$', 'student.views.register_user', name="register_user"),
     )
 
