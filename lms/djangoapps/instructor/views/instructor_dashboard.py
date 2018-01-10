cfd232e417327ef52d9edaf0b5a0ce251059b9bf
raccoondeploy <devops@raccoongang.com>
2018-01-10 13:51:30 +0000

    Fix ecomerce file downloading

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -117,7 +117,7 @@ def instructor_dashboard_2(request, course_id):
 
     is_white_label = CourseMode.is_white_label(course_key)
 
-    reports_enabled = configuration_helpers.get_value('SHOW_ECOMMERCE_REPORTS', False)
+    reports_enabled = configuration_helpers.get_value('SHOW_ECOMMERCE_REPORTS', True)
 
     sections = [
         _section_course_info(course, access),
