fc2f5f220d4720071c9c39151fc02f82a53a1ed7
Yura Braiko <yura.braiko@raccoongang.com>
2018-02-07 19:14:41 +0200

    Allow redirection only for the auth users

--- lms/djangoapps/ospp_api/urls.py
+++ lms/djangoapps/ospp_api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from ospp_api.views import CreateUserView, EnrollUserView, RoutView
 
 urlpatterns = (
     url(r'^create_user$', CreateUserView.as_view(), name='create_user'),
     url(r'^course_enrollments$', EnrollUserView.as_view(), name='course_enrollments'),
-    url(r'^rout_to/(?P<lms_page_name>\w+)/$', RoutView.as_view(), name='rout_to'),
+    url(r'^rout_to/(?P<lms_page_name>\w+)/$', login_required(RoutView.as_view()), name='rout_to'),
 )
