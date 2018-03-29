6c7066690d36562d734763368856c336b1b1c01b
Andrey Lykhoman <andrey.likhoman@gmail.com>
2018-03-29 18:12:54 +0300

    rebase setting for recaptcha to AUTH_TOKENS

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3239,10 +3239,3 @@ ARABIC_MONTHS = [
     "نوفمبر",
     "ديسمبر"
 ]
-
-############## Settings for CourseGraph ############################
-
-GOOGLE_RECAPTCHA_SECRET_KEY = ""
-GOOGLE_RECAPTCHA_DATA_SITE_KEY = ""
-USE_GOOGLE_RECAPTCHA = False
-
