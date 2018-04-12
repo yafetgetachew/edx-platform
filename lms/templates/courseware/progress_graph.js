70aa42e9e603e2e10403076b5c8562f1936266d1
Eslam Elgamal <me@esl4m.com>
2018-04-12 11:52:43 +0300

    Revert to hard translations

--- lms/templates/courseware/progress_graph.js
+++ lms/templates/courseware/progress_graph.js
@@ -117,7 +117,7 @@ $(function () {
             
             detail_tooltips[section['category'] + "-grade_breakdown"] = [ section['detail'] ]
   
-    ticks += [ [overviewBarX, _("TOTAL")] ]
+    ticks += [ [overviewBarX, "المجموع الكلي"] ]
     tickIndex += 1 + sectionSpacer
   
   totalScore = grade_summary['percent']
