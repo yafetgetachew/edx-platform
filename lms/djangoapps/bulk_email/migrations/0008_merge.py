be156008bb95b370814c05c78c1e3cba449fbf0c
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2018-04-12 06:50:07 -0400

    added migrations generated by './manage.py lms makemigrations'

---
+++ lms/djangoapps/bulk_email/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('bulk_email', '0007_auto_20180223_0355'),
+        ('bulk_email', '0007_auto_20171208_1943'),
+    ]
+
+    operations = [
+    ]
