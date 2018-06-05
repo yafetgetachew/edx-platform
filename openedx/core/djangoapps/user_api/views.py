c8be2e4f3000b6a1342c9d0cf07b9ec11d80e96d
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2018-06-05 10:06:55 +0300

    Hide registration form for SSO

--- openedx/core/djangoapps/user_api/views.py
+++ openedx/core/djangoapps/user_api/views.py
@@ -231,6 +231,7 @@ class RegistrationView(APIView):
             field_order = valid_fields
 
         self.field_order = field_order
+        self.current_provider = None
 
     @method_decorator(ensure_csrf_cookie)
     def get(self, request):
@@ -260,7 +261,7 @@ class RegistrationView(APIView):
         # Custom form fields can be added via the form set in settings.REGISTRATION_EXTENSION_FORM
         custom_form = get_registration_extension_form()
 
-        if custom_form:
+        if custom_form and not self.current_provider:
             # Default fields are always required
             for field_name in self.DEFAULT_FIELDS:
                 self.field_handlers[field_name](form_desc, required=True)
@@ -942,6 +943,7 @@ class RegistrationView(APIView):
             if running_pipeline:
                 current_provider = third_party_auth.provider.Registry.get_from_pipeline(running_pipeline)
 
+                self.current_provider = current_provider
                 if current_provider:
                     # Override username / email / full name
                     field_overrides = current_provider.get_register_form_data(
